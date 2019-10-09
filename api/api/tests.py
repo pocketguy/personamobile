6d5dc1165aabe6e6f33ef817cbd6319d4d4b47d5
nikita <nikita@carbonsoft.ru>
2019-10-09 18:28:51 +0300

    random img gen

--- api/api/tests.py
+++ api/api/tests.py
@@ -2,19 +2,24 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 from django.contrib.auth.models import User
-from PIL import Image
+from PIL import Image, ImageColor
 from io import BytesIO
+import random
 
 
 class MyTestCase(APITestCase):
     def setUp(self):
+        random.seed(42)
         User.objects.create_superuser("admin", "admin@example.com", "password")
 
     def login(self):
         self.assertTrue(self.client.login(username="admin", password="password"))
 
     def gen_image(self):
-        image = Image.new("RGB", (60, 30), color="red")
+        color = random.choice(list(ImageColor.colormap))
+        width = random.randrange(200, 300)
+        height = random.randrange(200, 300)
+        image = Image.new("RGB", (width, height), color=color)
         bio = BytesIO()
         image.save(bio, "PNG")
         bio.seek(0)
@@ -84,5 +89,4 @@ class FactoryTest(MyTestCase):
         }
         url = reverse("factory-list")
         response = self.client.post(url, data, format="json")
-        print(response.json())
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
