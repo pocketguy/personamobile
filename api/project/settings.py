816e22a549ea5f9da856915c359fd37c362f64fe
nikita <nikita@carbonsoft.ru>
2019-10-16 19:28:42 +0300

    override admin template

--- api/project/settings.py
+++ api/project/settings.py
@@ -30,13 +30,13 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    "api",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "api",
     "ckeditor",
     "ckeditor_uploader",
     "rest_framework",
