ffc63521aa4b124a2db86821da7a3c015283bb7d
nikita <nikita@carbonsoft.ru>
2019-10-18 16:15:33 +0300

    added scale to deploy script

--- prod/deploy.sh
+++ prod/deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 
+git pull
 docker-compose pull -q
-docker-compose up -d
+docker-compose up -d --scale backend=3 --scale web=3
 docker-compose run --rm backend ./manage.py migrate
 docker-compose run --rm backend ./manage.py collectstatic --noinput
